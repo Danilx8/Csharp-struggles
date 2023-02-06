9a99353fd4437cd3e7bb141382f7799dad4356d4
Danilx8 <lugovskihdanil@yandex.ru>
2023-02-06 23:23:00 +0700

    Wrote first program

---
+++ First Laba/First Laba/Power.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Laba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите степень:");
+            int Power = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Math.Exp(Math.Log(Number) * Power));
+            Console.ReadKey();
+        }
+    }
+}
