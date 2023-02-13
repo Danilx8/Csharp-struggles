be14af1377363a5f154aa851176227ee164e3743
Danilx8 <lugovskihdanil@yandex.ru>
2023-02-13 14:46:33 +0700

    Divercified the classes and added the word class

---
+++ SecondLaba/SecondLaba/MS Word class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SecondLaba
+{
+    class Word: Document
+    {
+        int RowCount;
+        int ColumnCount;
+
+        public override void Info()
+        {
+            Console.WriteLine("Name = ", Name, ";\n Author = ", Author, ";\n Keywords = ", KeyWords,
+                ";\n Topic = ", Topic, ";\n Path = ", Path, ";\n Rows amount: ", RowCount, ";\n Columns " +
+                "amount: ", ColumnCount);
+        }
+    }
+}
