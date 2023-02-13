fe036aa05cab68bc6e1e846beae0c480808894b4
Danilx8 <lugovskihdanil@yandex.ru>
2023-02-13 14:20:54 +0700

    Added a parental class, some changes to derived classes and a header comment

---
+++ SecondLaba/SecondLaba/SecondLab.cs
@@ -0,0 +1,49 @@
+﻿/*******************
+ *                 *
+ * Луговских Данил *
+ *    ПИ - 221     *
+ *      ООП        *
+ *                 *
+ *******************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondLaba
+{
+    internal class Program
+    {
+        class Document
+        {
+            string Name;
+            string Author;
+            string KeyWords;
+            string Topic;
+            string Path;
+
+            public virtual void Info()
+            {
+                Console.WriteLine("Name = ", Name, ";\n Author = ", Author, ";\n Keywords = ", KeyWords,
+                    ";\n Topic = ", Topic, ";\n Path = ", Path);
+            }
+        }
+
+        class Word: Document
+        {
+            int RowCount;
+            int ColumnCount;
+
+            public override void Info()
+            {
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
