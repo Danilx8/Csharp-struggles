782680d5f25cf33dd857babb15217385147d8c83
Danilx8 <lugovskihdanil@yandex.ru>
2023-02-23 17:58:32 +0700

    Init commit. Created an executable file and a class file

---
+++ Third Laba/Third Laba/Executable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Third_Laba
+{
+    internal class Executable
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(true.GetHashCode());
+            Console.ReadKey();
+        }
+    }
+}
