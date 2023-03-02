e824d73f48ab1dedb03a9a1f383b23b9edc81b1c
Danilx8 <lugovskihdanil@yandex.ru>
2023-03-03 01:13:11 +0700

    added a memento prototype and a search class. still can't catch what im going to do with local text files tbh

---
+++ Fourth Laba/Files searcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fourth_Laba
+{
+    internal class Searcher
+    {
+        private int count = 0;
+        private List<TextInfoClass> files = new List<TextInfoClass>();
+
+        public void AddFileToBase(FileStream file, string Keywords, string FileName)
+        {
+            TextInfoClass Newbie = new TextInfoClass(Keywords, FileName);
+            ++count;
+            files.Add(Newbie);
+        }
+
+        public TextInfoClass KeyWordsSearch(string KeyWords)
+        {
+            for (int ElementIndex = 0; ElementIndex < count; ++ElementIndex)
+            {
+                if (files[ElementIndex].Keywords == KeyWords)
+                {
+                    return files[ElementIndex];
+                }
+            }
+            return null;
+        }
+    }
+}
--- Fourth Laba/Text file.cs
+++ Fourth Laba/Text file.cs
@@ -1,18 +1,42 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Xml.Serialization;
 
 namespace Fourth_Laba
 {
-    internal class TextInfoClass
+    class Memento
     {
-        private string Keywords { get; set; }
+        public string Keywords { get; set; }
+        public string FileName { get; set; }
+    }
 
-        public TextInfoClass(string KeyWords)
+    public class Caretaker
+    {
+        private object memento;
+        public void SaveState(IOriginator originator)
         {
-            this.Keywords = KeyWords;
+            memento = originator.GetMemento();
+        }
+
+        public void RestoreState(IOriginator originator)
+        {
+            originator.SetMemento(memento);
+        }
+    }
+
+    [Serializable]
+    class TextInfoClass: IOriginator
+    {
+        public string Keywords { get; set; }
+        public string FileName { get; set; }
+
+        public TextInfoClass(string Keywords, string FileName)
+        {
+            this.Keywords = Keywords;
+            this.FileName = FileName;
         }
 
         public void BinarySerialization(FileStream fs) 
@@ -28,6 +52,7 @@ namespace Fourth_Laba
             BinaryFormatter bf = new BinaryFormatter();
             TextInfoClass deserialized = (TextInfoClass)bf.Deserialize(fs);
             Keywords = deserialized.Keywords;
+            FileName = deserialized.FileName;
             fs.Close();
         }
 
@@ -44,7 +69,23 @@ namespace Fourth_Laba
             XmlSerializer xmlserializer = new XmlSerializer(typeof(TextInfoClass));
             TextInfoClass deserialized = (TextInfoClass)xmlserializer.Deserialize(fs);
             Keywords = deserialized.Keywords;
+            FileName = deserialized.FileName;
             fs.Close();
+        }
+
+        object IOriginator.GetMemento()
+        {
+            return new Memento { Keywords = this.Keywords, FileName = this.FileName };
+        }
+
+        void IOriginator.SetMemento(object memento)
+        {
+            if (memento is Memento)
+            {
+                var mem = memento as Memento;
+                Keywords = mem.Keywords;
+                FileName = mem.FileName;
+            }
         }
     }
 }
