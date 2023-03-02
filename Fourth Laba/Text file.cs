933fc92dae25ca467c3aa1fc945966ac7bcc2718
Danilx8 <lugovskihdanil@yandex.ru>
2023-03-03 00:34:01 +0700

    added different serialization and deserialization methods

--- Fourth Laba/Text file.cs
+++ Fourth Laba/Text file.cs
@@ -1,12 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
 
 namespace Fourth_Laba
 {
-    internal class Class1
+    internal class TextInfoClass
     {
+        private string Keywords { get; set; }
+
+        public TextInfoClass(string KeyWords)
+        {
+            this.Keywords = KeyWords;
+        }
+
+        public void BinarySerialization(FileStream fs) 
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, this);
+            fs.Flush();
+            fs.Close();
+        }
+
+        public void BinaryDeserialization(FileStream fs) 
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            TextInfoClass deserialized = (TextInfoClass)bf.Deserialize(fs);
+            Keywords = deserialized.Keywords;
+            fs.Close();
+        }
+
+        public void XmlSerialization(FileStream fs) 
+        {
+            XmlSerializer xmlserializer = new XmlSerializer(typeof(TextInfoClass));
+            xmlserializer.Serialize(fs, this);
+            fs.Flush();
+            fs.Close();
+        }
+
+        public void XmlDeserialization(FileStream fs) 
+        {
+            XmlSerializer xmlserializer = new XmlSerializer(typeof(TextInfoClass));
+            TextInfoClass deserialized = (TextInfoClass)xmlserializer.Deserialize(fs);
+            Keywords = deserialized.Keywords;
+            fs.Close();
+        }
     }
 }
