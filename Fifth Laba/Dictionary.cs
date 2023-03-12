442b84f87a0d9b9dc4d93eb5846d3ff0ce8ce936
Danilx8 <lugovskihdanil@yandex.ru>
2023-03-12 18:16:17 +0700

    Created dictionary class

---
+++ Fifth Laba/Dictionary.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Fifth_Laba
+{
+    static class Dictionary
+    {
+        static Dictionary<string, List<string>> wrongWordsList = new Dictionary<string, List<string>>();
+        public static void SetDictionary(string Word, string[] wrongWords)
+        {
+            List<string> temp = new List<string>();
+            foreach(string currentWrongWord in wrongWords)
+            {
+                temp.Add(currentWrongWord);
+            }
+            wrongWordsList.Add(Word, temp);
+        }
+
+        public static List<string> GetList(string Key)
+        {
+            return wrongWordsList[Key];
+        }
+    }
+}
