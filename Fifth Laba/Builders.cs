da598f667bcd4c7445817a22a3bbd8bfbc120d7c
Danilx8 <lugovskihdanil@yandex.ru>
2023-03-14 09:28:53 +0700

    detached all dictionary setters to call them individually

--- Fifth Laba/Builders.cs
+++ Fifth Laba/Builders.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Fifth_Laba
 {
     interface IStringBuilder
     {
+        void CreateInstance(string Key, string[] WrongWords);
         void CreateMask(string Key);
         void Correct(string FilePath, string Model, string[] WrongWords);
     }
@@ -20,6 +22,11 @@ namespace Fifth_Laba
         public StringSearch()
         {
             Dictionary = new MyDictionary();
+        }
+
+        void IStringBuilder.CreateInstance(string Key, string[] WrongWords)
+        {
+            Dictionary.SetDictionary(Key, WrongWords);
         }
 
         void IStringBuilder.CreateMask(string Key)
@@ -63,7 +70,6 @@ namespace Fifth_Laba
 
         void IStringBuilder.Correct(string FilePath, string Model, string[] WrongWords)
         {
-            Dictionary.SetDictionary(Model, WrongWords);
             string Content = new StreamReader(FilePath).ReadToEnd();
             string Pattern = Dictionary.GetMask();
             Content = Regex.Replace(Content, Pattern, Model);
@@ -81,12 +87,20 @@ namespace Fifth_Laba
         {
             Dictionary = new MyDictionary();
         }
-
         
+        void IStringBuilder.CreateInstance(string Key, string[] WrongWords)
+        {
+            Dictionary.SetDictionary(Key, null);
+        }
+
+        void IStringBuilder.CreateMask(string Key)
+        {
+            Dictionary.SetMask("\\(\\d{3}\\)\\s\\d{3}-\\d{2}-\\d{2}");
+        }
 
         void IStringBuilder.Correct(string FilePath, string Model, string[] WrongWords)
         {
-
+            
         }
     }
 }
