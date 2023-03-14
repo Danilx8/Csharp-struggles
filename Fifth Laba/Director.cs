75e0a4e778ae41c159602a4df6c3dad794ccae17
Danilx8 <lugovskihdanil@yandex.ru>
2023-03-14 16:59:43 +0700

    created director class

---
+++ Fifth Laba/Director.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fifth_Laba
+{
+    internal class Director
+    {
+        private IStringBuilder StringBuilder;
+        public Director() { }
+        
+        public void SetBuilder(IStringBuilder StringBuilder)
+        {
+            this.StringBuilder = StringBuilder;
+        }
+
+        public void FixDocument(string FilePath, string Key)
+        {
+            StringBuilder.CreateInstance(Key);
+            StringBuilder.CreateMask(Key);
+            StringBuilder.Correct(FilePath);
+        }
+    }
+}
