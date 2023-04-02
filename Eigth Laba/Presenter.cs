124720fc55b4d32f748c45dbc567f5b118799dec
Danilx8 <lugovskihdanil@yandex.ru>
2023-04-02 10:45:12 +0700

    created the presenter class i guess

---
+++ Eigth Laba/Presenter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eigth_Laba
+{
+    internal class Presenter
+    {
+        private Model model;
+
+        public Presenter(string MainFolderPath, string SecondaryFolderPath)
+        {
+            model = new Model(MainFolderPath, SecondaryFolderPath);
+        }
+
+        public bool BootSynchronization(object sender, EventArgs e)
+        {
+            return (model.Synchronize());
+        }
+
+        public Dictionary<int, string[]> PassStates(object sender, EventArgs e)
+        {
+            return model.CheckStates();
+        }
+    }
+}
