34294bc973b616fde759386a9885646b75f77c58
Danilx8 <lugovskihdanil@yandex.ru>
2023-04-02 11:07:00 +0700

    Created view interface

---
+++ Eigth Laba/IView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eigth_Laba
+{
+    internal interface IView
+    {
+        string GetMainPath();
+        string GetSecondaryPath();
+        event EventHandler<EventArgs> Connect;
+        event EventHandler<EventArgs> Synchronize;
+    }
+}
